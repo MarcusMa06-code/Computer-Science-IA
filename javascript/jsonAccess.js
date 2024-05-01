89abace121a2ae8bbbb49330d01e09d370272796
Marcus <marcusma06@icloud.com>
2024-05-01 18:18:20 +0800

    made css and some js independet from htm file

---
+++ javascript/jsonAccess.js
@@ -0,0 +1,23 @@
+import studentsInfo from '../student_info.json' with {type: 'json'};
+
+const commonNameInput = document.getElementById('common-name')
+
+// auto complete the form
+commonNameInput.addEventListener('change',function(){
+
+    const grade = document.getElementById('grade').value;
+    const regClass = document.getElementById('register-class').value;
+
+    for(let i=0;i<studentsInfo.length;i++){
+        let studentInfo = studentsInfo[i];
+        if(studentInfo.grade === grade && studentInfo['common-name'] == commonNameInput.value){
+            document.getElementById('fname').value = studentInfo.fname;
+            document.getElementById('lname').value = studentInfo.lname;
+            document.getElementById('register-class').value = studentInfo['register-class'];
+            return;
+        }
+    }
+    alert(`No student ${commonNameInput.value} found.`)
+    commonNameInput.value = '';
+    commonNameInput.placeholder = 'Try again';
+})
